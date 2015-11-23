e8bae5d75c60f5b0e77611d100f85b18664bfcc2
Tsvetan Ganev <cecoganev94@hotmail.com>
2015-11-24 00:13:42 +0100

    Improves indentation.

--- TrainTicketBookingSystem/Migrations/Configuration.cs
+++ TrainTicketBookingSystem/Migrations/Configuration.cs
@@ -30,42 +30,42 @@ namespace TrainTicketBookingSystem.Migrations
             if (context.Cities.Count() == 0)
             {
                 context.Cities.AddOrUpdate(cities.ToArray());
+
                 // TrainRoutes
                 var trainRoutes = new List<TrainRoute>()
                 {
-                new TrainRoute { Departure = paris, Arrival = brussels, Price = 20.0M },
-                new TrainRoute { Departure = paris, Arrival = amsterdam, Price = 30.0M },
-                new TrainRoute { Departure = paris, Arrival = berlin, Price = 50.0M },
-                new TrainRoute { Departure = paris, Arrival = london, Price = 40.0M },
+                    new TrainRoute { Departure = paris, Arrival = brussels, Price = 20.0M },
+                    new TrainRoute { Departure = paris, Arrival = amsterdam, Price = 30.0M },
+                    new TrainRoute { Departure = paris, Arrival = berlin, Price = 50.0M },
+                    new TrainRoute { Departure = paris, Arrival = london, Price = 40.0M },
 
-                new TrainRoute { Departure = brussels, Arrival = amsterdam, Price = 20.0M },
-                new TrainRoute { Departure = brussels, Arrival = berlin, Price = 40.0M },
-                new TrainRoute { Departure = brussels, Arrival = london, Price = 30.0M },
-                new TrainRoute { Departure = brussels, Arrival = paris, Price = 30.0M },
+                    new TrainRoute { Departure = brussels, Arrival = amsterdam, Price = 20.0M },
+                    new TrainRoute { Departure = brussels, Arrival = berlin, Price = 40.0M },
+                    new TrainRoute { Departure = brussels, Arrival = london, Price = 30.0M },
+                    new TrainRoute { Departure = brussels, Arrival = paris, Price = 30.0M },
 
-                new TrainRoute { Departure = amsterdam, Arrival = london, Price = 30.0M },
-                new TrainRoute { Departure = amsterdam, Arrival = berlin, Price = 40.0M },
-                new TrainRoute { Departure = amsterdam, Arrival = paris, Price = 30.0M },
-                new TrainRoute { Departure = amsterdam, Arrival = brussels, Price = 20.0M },
+                    new TrainRoute { Departure = amsterdam, Arrival = london, Price = 30.0M },
+                    new TrainRoute { Departure = amsterdam, Arrival = berlin, Price = 40.0M },
+                    new TrainRoute { Departure = amsterdam, Arrival = paris, Price = 30.0M },
+                    new TrainRoute { Departure = amsterdam, Arrival = brussels, Price = 20.0M },
 
-                new TrainRoute { Departure = london, Arrival = berlin, Price = 60.0M },
-                new TrainRoute { Departure = london, Arrival = paris, Price = 40.0M },
-                new TrainRoute { Departure = london, Arrival = amsterdam, Price = 30.0M },
-                new TrainRoute { Departure = london, Arrival = brussels, Price = 30.0M },
+                    new TrainRoute { Departure = london, Arrival = berlin, Price = 60.0M },
+                    new TrainRoute { Departure = london, Arrival = paris, Price = 40.0M },
+                    new TrainRoute { Departure = london, Arrival = amsterdam, Price = 30.0M },
+                    new TrainRoute { Departure = london, Arrival = brussels, Price = 30.0M },
 
-                new TrainRoute { Departure = berlin, Arrival = london, Price = 60.0M },
-                new TrainRoute { Departure = berlin, Arrival = paris, Price = 50.0M },
-                new TrainRoute { Departure = berlin, Arrival = amsterdam, Price = 40.0M },
-                new TrainRoute { Departure = berlin, Arrival = brussels, Price = 40.0M },
+                    new TrainRoute { Departure = berlin, Arrival = london, Price = 60.0M },
+                    new TrainRoute { Departure = berlin, Arrival = paris, Price = 50.0M },
+                    new TrainRoute { Departure = berlin, Arrival = amsterdam, Price = 40.0M },
+                    new TrainRoute { Departure = berlin, Arrival = brussels, Price = 40.0M },
                };
 
                 context.TrainRoutes.AddOrUpdate(trainRoutes.ToArray());
 
                 // TRAINS
                 var dates = DateTimeGenerator.GetDaysInRange(DateTime.Today, DateTime.Today.AddDays(30));
-
-                //List<DateTime> dateTimes = new List<DateTime>();
                 var trains = new List<Train>();
+
                 foreach (DateTime date in dates)
                 {
                     for (int i = 0; i < 24; i++)
